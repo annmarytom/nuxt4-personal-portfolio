693a007c6e5c6ba32134f9673823e81ba336a50b
annmarytom <annmarytom9@gmail.com>
2026-01-16 01:20:34 +0400

    about page ui partially implemeted

---
+++ app/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <PageBanner pageBannerHeading="About"/>
+    </div>
+</template>
+<script setup>
+import PageBanner from '~/components/PageBanner.vue';
+
+
+
+
+</script>
+<style scoped>
+    h1 {
+      color: #333;
+    }
+    </style>
